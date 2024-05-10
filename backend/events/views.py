e103d532a5f55461fec72a1489bdc6728d04113d
Josue Martinez <103324696+josueemartinezz@users.noreply.github.com>
2024-05-09 23:57:04 -0700

    Commit: re-add HTTP methods

--- backend/events/views.py
+++ backend/events/views.py
@@ -1,8 +1,8 @@
-from rest_framework import viewsets
-from rest_framework.throttling import (
-    AnonRateThrottle,
-    UserRateThrottle,
-)
+from rest_framework import status, viewsets
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.request import Request
+from rest_framework.throttling import AnonRateThrottle, UserRateThrottle
 
 from backend.paginator import CustomPagination
 
@@ -39,6 +39,83 @@ class EventViewSet(viewsets.ModelViewSet[Event]):
     serializer_class = EventSerializer
     pagination_class = CustomPagination
     throttle_classes = [AnonRateThrottle, UserRateThrottle]
+    permission_classes = [
+        IsAuthenticated,
+    ]
+    
+    def list(self, request: Request) -> Response:
+        serializer = self.get_serializer(self.queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def create(self, request: Request) -> Response:
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(created_by=request.user)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def retrieve(self, request: Request, pk: str | None = None) -> Response:
+        event = self.queryset.filter(id=pk).first()
+        if event:
+            serializer = self.get_serializer(event)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        
+        return Response({"error": "Event not found"}, status.HTTP_404_NOT_FOUND)
+
+    def update(self, request: Request, pk: str | None = None) -> Response:
+        event = self.queryset.filter(id=pk).first()
+        if event is None:
+            return Response(
+                {"error": "Event not found"}, status.HTTP_404_NOT_FOUND
+            )
+
+        if request.user != event.created_by:
+            return Response(
+                {"error": "You are not authorized to update this event"},
+                status.HTTP_401_UNAUTHORIZED,
+            )
+
+        serializer = self.get_serializer(event, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status.HTTP_200_OK)
+
+    def partial_update(self, request: Request, pk: str | None = None) -> Response:
+        event = self.queryset.filter(id=pk).first()
+        if event is None:
+            return Response(
+                {"error": "Event not found"}, status.HTTP_404_NOT_FOUND
+            )
+
+        if request.user != event.created_by:
+            return Response(
+                {"error": "You are not authorized to update this event"},
+                status.HTTP_401_UNAUTHORIZED,
+            )
+
+        serializer = self.get_serializer(event, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status.HTTP_200_OK)
+
+    def destroy(self, request: Request, pk: str | None = None) -> Response:
+        event = self.queryset.filter(id=pk).first()
+        print(pk, event)
+        if event is None:
+            return Response(
+                {"error": "Event not found"}, status.HTTP_404_NOT_FOUND
+            )
+
+        if request.user != event.created_by:
+            return Response(
+                {"error": "You are not authorized to delete this event"},
+                status.HTTP_401_UNAUTHORIZED,
+            )
+
+        event.delete()
+        
+        return Response(
+            {"message": "Event deleted successfully"}, status.HTTP_200_OK
+        )
 
 
 class FormatViewSet(viewsets.ModelViewSet[Format]):
